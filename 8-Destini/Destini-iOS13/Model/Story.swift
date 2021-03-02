e4cc309dae59b1d2ec3684af97986d6c2a714976
saidulislam <saidul.islam@gmail.com>
2021-03-02 08:18:29 -0500

    fully working version of destini

--- 8-Destini/Destini-iOS13/Model/Story.swift
+++ 8-Destini/Destini-iOS13/Model/Story.swift
@@ -2,8 +2,15 @@
 //  Sotry.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Saidul Islam.
 //
 
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
